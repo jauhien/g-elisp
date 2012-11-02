5671fca31096b7859cbfc1f4dbc1c51fa612a5de
Jauhien Piatlicki (jauhien) <piatlicki@gmail.com>
2012-11-03 00:51:08 +0100

    setup

---
+++ g_elisp/version.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+VERSION = '0'
+
+if __name__ == '__main__':
+    print VERSION
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from g_elisp.version import VERSION
+
+setup(name          = 'g-elisp',
+      version       = VERSION,
+      description   = 'Driver for g-common repository of emacs packages',
+      author        = 'Jauhien Piatlicki',
+      author_email  = 'piatlicki@gmail.com',
+      packages      = ['g_elisp'],
+      scripts       = ['bin/g-elisp'],
+      license       = 'GPL',
+      )
