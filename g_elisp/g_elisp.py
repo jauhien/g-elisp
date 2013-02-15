102b16f66bdacfd922db175e8fc6b484d0e22e7a
Jauhien Piatlicki (jauhien) <piatlicki@gmail.com>
2013-02-15 15:58:36 +0100

    typo

--- g_elisp/g_elisp.py
+++ g_elisp/g_elisp.py
@@ -5,7 +5,7 @@ import sys, collections, os
 from urllib import request
 from urllib.parse import urljoin
 
-from g_elisp.files import get_datadir, get_arcfile, get_cfgfile, get_pkgpath
+from g_elisp.files import get_datadir, get_arcfile, get_cfgfile, get_eclfile
 from g_elisp.parsers import parse_repo, parse_args, Argument, Arguments, Command_group, Command
 
 def get_GAPI(args):
