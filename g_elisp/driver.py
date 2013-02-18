bf48e4ca1c1e85fe98a4096cf08241c40bb82ece
Jauhien Piatlicki (jauhien) <piatlicki@gmail.com>
2013-02-18 19:34:28 +0100

    ebuild src

--- g_elisp/driver.py
+++ g_elisp/driver.py
@@ -46,18 +46,39 @@ class GELispDriver(Driver):
         return 0
 
     def ebuild_src(self, args):
+        self.overlay = args.overlay
         o_arc = ArchiveContents(self.overlay)
         repo = o_arc.parse()
+        o_cfg = GELispOverlayConfig(self.overlay)
+        o_cfg.read()
         print("""# Copyright 1999-2013 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 # $Header: $
 
 EAPI=5
 
-SLOT="0"
+inherit g-elisp
 
-DESCRIPTION="test"
-HOMEPAGE="http://test.org"
-SRC_URI=""
 """)
+        pkg = Package(args.name.split('/')[1], args.version)
+        attr = repo[pkg]
+        uri = o_cfg.cfg['overlay']['uri']
+        print('DESCRIPTION="' + attr.desc + '"')
+        print('HOMEPAGE="' + uri + '"')
+        print('SRC_URI=""')
+        print('LICENSE="GPL-2"')
+        print()
+        print('SLOT="0"')
+        print('KEYWORDS="~amd64 ~x86"')
+        print('IUSE=""')
+        print('')
+        print('REPO_URI="' + uri + '"')
+        print('PKG_TYPE="' + attr.type + '"')
+        print()
+        deps = ""
+        for i in attr.deps:
+            deps += "\n\tapp-emacs/" + i.name + "-" + i.version
+        print('DEPEND="${DEPEND}' + deps + '"')
+        print('RDEPEND="${DEPEND}"')
+        print()
         return 0
