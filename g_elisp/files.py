381d6c1a3ac9106071a084a888b4c078dbf6bdcc
Jauhien Piatlicki (jauhien) <piatlicki@gmail.com>
2013-02-20 18:44:33 +0100

    clean

--- g_elisp/files.py
+++ g_elisp/files.py
@@ -45,47 +45,6 @@ class ArchiveContents(File):
             f.write(h.read())
         h.close()
         self.cached_read()
-        
-
-## import os
-## from urllib import request
-## from urllib.parse import urljoin
-
-## from g_common.files import ConfigFile
-
-## from g_elisp.parsers import repo
-
-## DATADIR = ".g-elisp"
-## ARCFILE = "archive-contents"
-## CFGFILE = "overlay.cfg"
-## ECLFILE = "data/g-elisp.eclass"
-
-## class GELispOverlayConfig(ConfigFile):
-##     def __init__(self, overlay):
-##         super().__init__(os.path.join(overlay, DATADIR), CFGFILE)
-
-## class ArchiveContents:
-##     def __init__(self, overlay):
-##         self.name = ARCFILE
-##         self.directory = os.path.join(overlay, DATADIR)
-##         self.path = os.path.join(self.directory, ARCFILE)
-
-##     def sync(self, uri):
-##         if not os.path.exists(self.directory):
-##             os.makedirs(self.directory)
-##         archive_contents = urljoin(uri, 'archive-contents')
-##         h = request.urlopen(archive_contents)
-##         f = open(self.path, 'wb')
-##         f.write(h.read())
-##         h.close()
-##         f.close()
-##         return 0
-
-##     def parse(self):
-##         with open(self.path, 'r') as arcfile:
-##             self.repo = repo.parseFile(arcfile).asDict()
-##         return self.repo
-
 
 def get_pkgpath():
      root = __file__
